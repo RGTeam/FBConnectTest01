5a165ef42b609a527f24464df7c26746ae28c493
Roberto Ghizzi <r.ghizzi@gmail.com>
2013-02-27 22:29:38 +0100

    Created Project

---
+++ Test01/Test01/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Test01
+//
+//  Created by Roberto Ghizzi on 27/02/13.
+//  Copyright (c) 2013 Roberto Ghizzi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Test01/Test01/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Test01
+//
+//  Created by Roberto Ghizzi on 27/02/13.
+//  Copyright (c) 2013 Roberto Ghizzi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
